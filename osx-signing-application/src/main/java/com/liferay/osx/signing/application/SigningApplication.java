19fefb263ca27c9a12ef077288462ded681dfe14
Gregory Amerson <gregory.amerson@liferay.com>
2017-07-10 22:51:28 -0500

    Finish working codesign and verify methods

--- osx-signing-application/src/main/java/com/liferay/osx/signing/application/SigningApplication.java
+++ osx-signing-application/src/main/java/com/liferay/osx/signing/application/SigningApplication.java
@@ -1,8 +1,23 @@
 package com.liferay.osx.signing.application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 
@@ -22,12 +37,143 @@ public class SigningApplication {
 
 	@POST
 	@Path("/codesign")
-	public String codeSign(@PathParam("name") String appPath) {
-		if (_log.isDebugEnabled()) {
-			_log.debug("URI: " + _uriInfo.getAbsolutePath());
+	public String codesign(@FormParam("identity")String identity, @FormParam("path")String path) {
+		_ensureBinariesAvailable();
+
+		final File dir;
+
+		try {
+			dir = _getDir(path);
+		} catch (Exception e) {
+			_log.error(e.getMessage(), e);
+
+			return "ERROR: " + e.getMessage();
 		}
 
-		return "codesign: appPath=" + appPath;
+		try {
+			_sign(dir, identity);
+
+			List<String> output = _verify(dir);
+
+			return _toString(output);
+		} catch (Exception e) {
+			_log.error("Error signing application", e);
+
+			return "ERROR: " + e.getMessage();
+		}
+	}
+
+	@GET
+	@Path("/verify")
+	public String verify(@QueryParam("path") String path) {
+		_ensureBinariesAvailable();
+
+		final File dir;
+
+		try {
+			dir = _getDir(path);
+		} catch (Exception e) {
+			_log.error(e.getMessage(), e);
+
+			return "ERROR: " + e.getMessage();
+		}
+
+		try {
+			List<String> output = _verify(dir);
+
+			return _toString(output);
+		} catch (Exception e) {
+			_log.error("Error signing application", e);
+
+			return "ERROR: " + e.getMessage();
+		}
+	}
+
+	private List<String> _sign(File dir, String identity) throws IOException, InterruptedException {
+		// codesign --deep -s "Developer ID Application: Liferay, Inc." -f DeveloperStudio.app/
+
+		Process signProcess = new ProcessBuilder().
+			command("codesign", "--deep", "-s", identity, dir.getAbsolutePath()).
+			start();
+
+		List<String> stderr = _readStreamFully(signProcess.getErrorStream());
+
+		if (stderr.size() > 0) {
+			throw new RuntimeException(_toString(stderr));
+		}
+
+	    int exitCode = signProcess.waitFor();
+
+	    if (exitCode > 0) {
+	    	throw new RuntimeException("codesign returned error code: " + _toString(stderr));
+	    }
+
+	    return stderr;
+	}
+
+	private List<String> _verify(File dir) throws Exception {
+		// codesign -dv --verbose=4 DeveloperStudio.app/
+
+		Process verifyProcess = new ProcessBuilder().
+				command("codesign", "-dv", "--verbose=4", dir.getAbsolutePath()).
+				start();
+
+		List<String> stderr = _readStreamFully(verifyProcess.getErrorStream());
+
+	    int exitCode = verifyProcess.waitFor();
+
+	    if (exitCode > 0) {
+	    	throw new RuntimeException("codesign returned error\n: " + _toString(stderr));
+	    }
+
+	    return stderr;
+	}
+
+	private String _toString(List<String> strings) {
+		return strings.stream().collect(Collectors.joining("\n"));
+	}
+
+	private List<String> _readStreamFully(InputStream inputStream) throws IOException {
+		List<String> retval = new ArrayList<>();
+
+		InputStreamReader isr = new InputStreamReader(inputStream);
+	    BufferedReader br = new BufferedReader(isr);
+
+	    String line;
+
+	    while ((line = br.readLine()) != null) {
+	    	retval.add(line);
+	    }
+
+		return retval;
+	}
+
+
+	private void _ensureBinariesAvailable() {
+		if (!_findExecutable("codesign")) {
+			throw new RuntimeException("Could not find executable: codesign");
+		}
+
+		if (!_findExecutable("security")) {
+			throw new RuntimeException("Could not find executable: security");
+		}
+	}
+
+	private boolean _findExecutable(String exec) {
+		return Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator)))
+	        .map(Paths::get)
+	        .anyMatch(path -> Files.exists(path.resolve(exec)));
+	}
+
+
+	private File _getDir(String path) throws Exception {
+		File dir = new File(path);
+
+		if (dir.exists() && dir.isDirectory()) {
+			return dir;
+		}
+
+		throw new IllegalArgumentException(path + " is not an existing directory.");
 	}
 
 	@Context
--- osx-signing-test/src/main/java/com/liferay/osx/signing/test/SigningApplicationTest.java
+++ osx-signing-test/src/main/java/com/liferay/osx/signing/test/SigningApplicationTest.java
@@ -1,11 +1,153 @@
 package com.liferay.osx.signing.test;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+import aQute.lib.io.IO;
 
 public class SigningApplicationTest {
 
-	@Test
-	public void testSomething() {
+	private static final BundleContext bundleContext = FrameworkUtil.getBundle(SigningApplicationTest.class)
+			.getBundleContext();
 
+	private ServiceTracker<ClientBuilder, ClientBuilder> _clientBuilderTracker;
+
+	@After
+	public void after() {
+		_clientBuilderTracker.close();
+	}
+
+	@Before
+	public void before() {
+		_clientBuilderTracker = new ServiceTracker<>(bundleContext, ClientBuilder.class, null);
+
+		_clientBuilderTracker.open();
+	}
+
+	@Test
+	public void testCodesignBadIdentity() throws Exception {
+		Path tmpAppDir = _extractTestApp("unsigned/LiferayWorkspace-1.5.0-osx-installer.app");
+
+		MultivaluedHashMap<String, String> form = new MultivaluedHashMap<String,String>();
+		form.add("identity", "foo");
+		form.add("path", tmpAppDir.toString());
+
+		Response response = _createClient()
+			.target("http://localhost:8888")
+			.path("/codesign")
+			.request()
+			.post(Entity.form(form));
+
+		Assert.assertNotNull(response);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+		String postResponse = response.readEntity(String.class);
+
+		Assert.assertEquals("ERROR: foo: no identity found", postResponse);
+	}
+
+	@Test
+	public void testVerifySigned() throws Exception {
+		Path tmpAppDir = _extractTestApp("signed/LiferayWorkspace-1.5.0-osx-installer.app");
+
+		Response response = _createClient()
+			.target("http://localhost:8888")
+			.path("/verify")
+			.queryParam("path", tmpAppDir.toAbsolutePath().toString())
+			.request()
+			.get(Response.class);
+
+		Assert.assertNotNull(response);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+		String getResponse = response.readEntity(String.class);
+
+		Assert.assertTrue(Stream.of(getResponse.split("\n")).anyMatch(s -> s.contains("Developer ID Application: Liferay, Inc. (7H3SPU5TB9)")));
+	}
+
+	@Test
+	public void testVerifyUnSigned() throws Exception {
+		Path tmpAppDir = _extractTestApp("unsigned/LiferayWorkspace-1.5.0-osx-installer.app");
+
+		Response response = _createClient()
+			.target("http://localhost:8888")
+			.path("/verify")
+			.queryParam("path", tmpAppDir.toAbsolutePath().toString())
+			.request()
+			.get(Response.class);
+
+		Assert.assertNotNull(response);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+		String getResponse = response.readEntity(String.class);
+
+		Assert.assertTrue(getResponse, getResponse.contains("code object is not signed at all"));
+	}
+
+	@Test
+	public void testCodesign() throws Exception {
+		Path tmpAppDir = _extractTestApp("unsigned/LiferayWorkspace-1.5.0-osx-installer.app");
+
+		MultivaluedHashMap<String, String> form = new MultivaluedHashMap<String,String>();
+		form.add("identity", "Developer ID Application: Liferay, Inc. (7H3SPU5TB9)");
+		form.add("path", tmpAppDir.toString());
+
+		Response response = _createClient()
+			.target("http://localhost:8888")
+			.path("/codesign")
+			.request()
+			.post(Entity.form(form));
+
+		Assert.assertNotNull(response);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+		String postResponse = response.readEntity(String.class);
+
+		Assert.assertTrue(Stream.of(postResponse.split("\n")).anyMatch(s -> s.contains("Developer ID Application: Liferay, Inc. (7H3SPU5TB9)")));
+	}
+
+	private Path _extractTestApp(String path) throws Exception {
+		Path tmpDir = Files.createTempDirectory("temp-app-dir");
+
+		IO.copy(new File("src/test/resources"), tmpDir.toFile());
+
+		Path appPath = tmpDir.resolve(path);
+
+		Assert.assertTrue(appPath.toString(), appPath.toFile().exists());
+
+		return appPath;
+	}
+
+	private Client _createClient() {
+		ClientBuilder clientBuilder;
+
+		try {
+			clientBuilder = _clientBuilderTracker.waitForService(5000);
+
+			return clientBuilder.build();
+		} catch (InterruptedException ie) {
+			throw new RuntimeException(ie);
+		}
 	}
 }
